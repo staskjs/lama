5586200fafd16b90978e6b43db3782c40f3c8c48
Stas Karpov <gilbert_90@mail.ru>
2016-04-05 17:56:58 +0300

    fix wrong engine name

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -258,7 +258,7 @@ Devise.setup do |config|
   #     mount MyEngine, at: '/my_engine'
   #
   # The router that invoked `devise_for`, in the example above, would be:
-   config.router_name = :my_engine
+   config.router_name = :lama
   #
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
