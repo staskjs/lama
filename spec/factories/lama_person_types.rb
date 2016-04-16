32a90323ed736315d9c53fb8c312ef89ef085c4b
Stas Karpov <gilbert_90@mail.ru>
2016-04-16 14:09:34 +0300

    [style] rubocop fix

--- spec/factories/lama_person_types.rb
+++ spec/factories/lama_person_types.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
-  factory :lama_person_type, :class => 'Lama::PersonType' do
+  factory :lama_person_type, class: 'Lama::PersonType' do
     name { Faker::Lorem.word }
     active true
   end
-
 end
